4cd10c72d599909f25e816a3b307513995f1fe8e
Hadley Wickham <h.wickham@gmail.com>
2020-05-20 15:49:27 -0500

    Function to determine if port is active

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,12 @@
 file_mtime <- function(path) {
   file_info(path)$change_time
 }
+
+port_active <- function(port) {
+  tryCatch({
+    suppressWarnings(con <- socketConnection(port = port))
+    close(con)
+    TRUE
+  }, error = function(e) FALSE)
+}
+
