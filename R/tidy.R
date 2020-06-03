d5686ca900c567bd80df017b86d3558c4aab81de
Hadley Wickham <h.wickham@gmail.com>
2020-06-03 16:18:46 -0500

    Eliminate utils.R

--- R/tidy.R
+++ R/tidy.R
@@ -131,3 +131,17 @@ tidy_pleased <- function() {
 
   paste0(modifier, if (modifier != "") " ", word)
 }
+
+active_file <- function(ext = NULL) {
+  if (!rstudioapi::isAvailable()) {
+    abort("Must supply `path` outside of RSuudio")
+  }
+
+  path <- rstudioapi::getSourceEditorContext()$path
+
+  if (!is.null(ext) && path_ext(path) != ext) {
+    abort(paste0("Open file must have extension (", ext, ")"))
+  }
+
+  path
+}
