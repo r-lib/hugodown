d7b18d4c9261db53145ae7d915026673e4096238
Hadley Wickham <h.wickham@gmail.com>
2020-06-07 08:48:33 -0500

    Use whoami to determine name
    Fixes #33

--- R/post.R
+++ R/post.R
@@ -7,12 +7,16 @@
 #' and then uses [whisker](https://github.com/edwindj/whisker) to template
 #' any `.Rmd` files.
 #'
+#'
 #' @param path Directory to create, like `blog/2020-my-favourite-package`.
 #' @param kind Kind of archetype of use; usually automatically derived
 #'   from the base directory of `path`.
 #' @param data Any additional data to be used when templating `.Rmd` files.
-#'   The default set `date` to today's date (in YYYY-MM-DD format), and
-#'   author to your name (if set in the `usethis.full_name` option).
+#'
+#'   The default data includes:
+#'   * `date`: today's date (in YYYY-MM-DD format).
+#'   * `author`: [whoami::fullname()].
+#'   * `slug`: taken from the file name of `path`.
 #' @param site Path to the hugo site.
 #' @param open Open file for interactive editing?
 #' @export
@@ -38,7 +42,7 @@ use_post <- function(path, kind = NULL, data = list(), site = ".", open = is_int
   rmds <- dir_ls(dest, glob = "*.Rmd")
   defaults <- list(
     slug = path_file(path),
-    author = find_name(),
+    author = whoami::fullname("Your name"),
     date = strftime(Sys.Date(), "%Y-%m-%d")
   )
   data <- utils::modifyList(defaults, data)
@@ -55,11 +59,4 @@ rmd_template <- function(path, data) {
   file <- brio::read_file(path)
   out <- whisker::whisker.render(file, data)
   brio::write_lines(out, path)
-}
-
-
-# Helpers -----------------------------------------------------------------
-
-find_name <- function() {
-  getOption("usethis.full_name") %||% getOption("devtools.name") %||% "Your name"
 }
