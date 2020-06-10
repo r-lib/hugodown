12be152fa92aa4cd9e9689a943c9f9657a0c8e9e
Hadley Wickham <h.wickham@gmail.com>
2020-06-10 17:39:37 -0500

    New local_render() test fixture
    To reduce repetition in tests

--- tests/testthat/test-md-document.R
+++ tests/testthat/test-md-document.R
@@ -5,110 +5,79 @@ test_that("errors are handled gracefully", {
 })
 
 test_that("figures placed in figs/ directory", {
-  rmd <- local_file(test_path("plot.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
+  rmd <- local_render(test_path("plot.Rmd"))
 
-  figs <- path(path_dir(rmd), "figs")
+  figs <- path(rmd$dir, "figs")
   expect_true(dir_exists(figs))
   expect_equal(length(dir_ls(figs)), 2L)
 
   # Check we're not converting percentage widths to latex
-  lines <- brio::read_lines(path(path_dir(rmd), "plot.md"))
-  expect_match(lines[[15]], 'width="50%"')
+  expect_match(rmd$lines[[15]], 'width="50%"')
 })
 
 test_that("tables use pipes", {
-  rmd <- local_file(test_path("table.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "table.md")
-
-  lines <- brio::read_lines(out)
-  expect_equal(sum(grepl("|", lines, fixed = TRUE)), 4)
+  rmd <- local_render(test_path("table.Rmd"))
+  expect_equal(sum(grepl("|", rmd$lines, fixed = TRUE)), 4)
 })
 
 test_that("code is linked/highlighted", {
-  rmd <- local_file(test_path("code.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "code.md")
+  rmd <- local_render(test_path("code.Rmd"))
 
-  lines <- brio::read_lines(out)
-  expect_equal(sum(grepl("<pre", lines, fixed = TRUE)), 2)
-  expect_equal(sum(grepl("[`stats::median()`]", lines, fixed = TRUE)), 1)
+  expect_equal(sum(grepl("<pre", rmd$lines, fixed = TRUE)), 2)
+  expect_equal(sum(grepl("[`stats::median()`]", rmd$lines, fixed = TRUE)), 1)
 })
 
 test_that("output gets unicode and colour", {
   skip_on_os("windows")
-  rmd <- local_file(test_path("output.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  lines <- brio::read_lines(path(path_dir(rmd), "output.md"))
 
-  expect_match(lines[[9]], "color: #0000BB")
-  expect_match(lines[[10]], "#&gt; \u2714")
+  rmd <- local_render(test_path("output.Rmd"))
+  expect_match(rmd$lines[[9]], "color: #0000BB")
+  expect_match(rmd$lines[[10]], "#&gt; \u2714")
 })
 
 test_that("interweaving of code and output generates correct html", {
-  rmd <- local_file(test_path("knit-hooks.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- brio::read_lines(path(path_dir(rmd), "knit-hooks.md"))
-  verify_output(test_path("test-md-document-hooks.txt"), cat_line(out))
+  rmd <- local_render(test_path("knit-hooks.Rmd"))
+  verify_output(test_path("test-md-document-hooks.txt"), cat_line(rmd$lines))
 })
 
 test_that("markdown div syntax is converted to native divs", {
-  rmd <- local_file(test_path("div.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "div.md")
-
-  lines <- brio::read_lines(out)
-  expect_equal(sum(grepl("div", lines, fixed = TRUE)), 2)
+  rmd <- local_render(test_path("div.Rmd"))
+  expect_equal(sum(grepl("div", rmd$lines, fixed = TRUE)), 2)
 })
 
 test_that("emojis are preserved", {
-  rmd <- local_file(test_path("emoji.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "emoji.md")
-
-  lines <- brio::read_lines(out)
-  expect_equal(lines[[7]], ":smile_cat:")
+  rmd <- local_render(test_path("emoji.Rmd"))
+  expect_equal(rmd$lines[[7]], ":smile_cat:")
 })
 
 test_that("math is untransformed", {
-  rmd <- local_file(test_path("math.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "math.md")
-
-  lines <- brio::read_lines(out)
-  expect_equal(lines[length(lines) - 1], "$a_1 + b_2$")
+  rmd <- local_render(test_path("math.Rmd"))
+  expect_equal(rmd$lines[[7]], "$a_1 + b_2$")
 })
 
 test_that("hash added to yaml header", {
-  rmd <- local_file(test_path("meta.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "meta.md")
+  rmd <- local_render(test_path("meta.Rmd"))
 
-  yaml <- rmarkdown::yaml_front_matter(rmd)
-  yaml$rmd_hash <- rmd_hash(rmd)
-  expect_equal(rmarkdown::yaml_front_matter(out), yaml)
+  yaml <- rmarkdown::yaml_front_matter(rmd$src)
+  yaml$rmd_hash <- rmd_hash(rmd$src)
+  expect_equal(rmarkdown::yaml_front_matter(rmd$dst), yaml)
 
   # Test that yaml is preserved as is (i.e. no round-tripping)
-  lines <- brio::read_lines(out)
-  expect_equal(lines[[4]], "# this is a comment")
+  expect_equal(rmd$lines[[4]], "# this is a comment")
 })
 
 test_that("html dependencies are captured", {
-  rmd <- local_file(test_path("widget.Rmd"))
-  rmarkdown::render(rmd, quiet = TRUE)
-  out <- path(path_dir(rmd), "widget.md")
+  rmd <- local_render(test_path("widget.Rmd"))
 
   # Have copied over dependencies
-  widget_js <- path(path_dir(rmd), paste0("htmlwidgets-", packageVersion("htmlwidgets")))
+  widget_js <- path(rmd$dir, paste0("htmlwidgets-", packageVersion("htmlwidgets")))
   expect_true(dir_exists(widget_js))
 
   # And written in yaml metadata
-  yaml <- rmarkdown::yaml_front_matter(out)
+  yaml <- rmarkdown::yaml_front_matter(rmd$dst)
   expect_type(yaml$html_dependencies, "character")
   expect_true(length(yaml$html_dependencies) > 1)
 })
-
 
 # helpers -----------------------------------------------------------------
 
