46c908383a0f280e0fb468187235a87247bef5e9
Hadley Wickham <h.wickham@gmail.com>
2020-06-11 09:40:45 -0500

    Document and export shortcode

--- R/shortcode.R
+++ R/shortcode.R
@@ -1,3 +1,30 @@
+#' Generate a hugo shortcode
+#'
+#' @description
+#' Generate a hugo shortcode with appropriate pandoc markup to preserve it as
+#' is when embedded in an R markdown document.
+#'
+#' Generally, I don't recommend calling this function directly; instead
+#' use it inside a function with the same name as the shortcode you want to
+#' wrap. See [embed_gist()] and friends for examples
+#'
+#' @param .name Nane of the shortcode
+#' @param ... Arguments to the shortcode, supplied either by name or
+#'   position depending on the shortcode. By default, strings will
+#'   automatically be quoted with single quotes. Suppress this quoting
+#'   by wrapping the argument in `I()`.
+#' @param .contents Contents of the shortcode for paired shortcodes.
+#' @param .output Is the output of the shortcode html or markdown? This
+#'   controls whether the shortcode uses `<>` or `%`.
+#' @param .inline Is the shortcode designed to be used inline or in its own
+#'   paragraph? Controls whether the shortcode is wrapped in a block or inline
+#'   [raw attribute](https://pandoc.org/MANUAL.html#extension-raw_attribute).
+#' @export
+#' @examples
+#' pkg <- function(name) {
+#'   shortcode("pkg", name, .inline = TRUE)
+#' }
+#' pkg("hugodown")
 shortcode <- function(.name, ..., .contents = NULL, .output = c("html", "md"), .inline = FALSE) {
   call <- paste0(c(.name, shortcode_args(...)), collapse = " ")
   wrap <- switch(arg_match(.output),
